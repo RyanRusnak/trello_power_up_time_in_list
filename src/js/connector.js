b67de8d6ba544a195f72729e5b8f8fa799d539b9
ryanrusnak <rusnak@vt.edu>
2021-01-04 15:26:39 -0800

    returning card

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,7 +3,12 @@ console.log('hello world');
 window.TrelloPowerUp.initialize({
 	'card-badges': function(t, opts){
 		// return an array of card badges
-		return [];
-
+		return t.card('all')
+		.then(function(card){
+			consle.log(card);
+			return [{
+				text: card.idShort
+			}];
+		})
 	}
 });
