5ef745d3043f7d2d088922d29f296d75853577df
ryanrusnak <rusnak@vt.edu>
2021-01-05 16:38:40 -0800

    adding HTTP request to trello

--- src/js/connector.js
+++ src/js/connector.js
@@ -6,7 +6,14 @@ window.TrelloPowerUp.initialize({
 		return t.card('all')
 		.then(function(card){
 			console.log(card);
+			// request array of card activity
+			fetch('https://api.trello.com/1/members/me/boards?key=%%TRELLO_KEY%%&token=%%TRELLO_TOKEN%%')
+			.then(response => response.json())
+			.then(data => console.log(data));
+			// find most recent date card entered column
+			// figure out days between today and entry date
 			return [{
+				// return days
 				text: card.idShort
 			}];
 		})
