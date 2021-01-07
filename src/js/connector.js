a698db71f43f0a946b4bfefb50f595fa69a5f1b9
ryanrusnak <rusnak@vt.edu>
2021-01-06 17:33:52 -0800

    handling 0 activity

--- src/js/connector.js
+++ src/js/connector.js
@@ -6,29 +6,22 @@ window.TrelloPowerUp.initialize({
 		return t.card('all')
 		.then(function(card){
 			var cardActions = [];
-			console.log(card);
 			// request array of card activity
 			return fetch(`https://api.trello.com/1/cards/${card.id}/actions?key=%%TRELLO_KEY%%&token=%%TRELLO_TOKEN%%`)
 			.then(response => response.json())
 			.then(data => {
-				/////////////
-				// handle if there is no activity
-				// should just result in today - card creation date
-				/////////////
-				// find most recent date card entered column
 				var cardMoves = data.filter(isCardMoveAction);
+				// put 0 days if there is no activity history
 				if (cardMoves.length) == 0 {
 					return 0
 				}
-				var mostRecentMove = cardMoves[0]
 				// figure out days between today and entry date
-				return Math.round((Date.now() - Date.parse(mostRecentMove.date)) / 171701012);
-			}).then(function(cardData){
-				console.log('Printing card data');
-				console.log(cardData);
+				return Math.round((Date.now() - Date.parse(cardMoves[0].date)) / 171701012);
+			}).then(function(days){
+				console.log(days);
 				return [{
 					// return days
-					text: cardData + ' days'
+					text: days + ' days'
 				}]
 			});
 			return [];
