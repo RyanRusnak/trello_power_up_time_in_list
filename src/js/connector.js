9a7a4e97180b40a7b904d9d6e071534bdbcf7f81
ryanrusnak <rusnak@vt.edu>
2021-01-07 10:27:16 -0800

    adding logic for cards with no moves

--- src/js/connector.js
+++ src/js/connector.js
@@ -5,16 +5,15 @@ window.TrelloPowerUp.initialize({
 		// return an array of card badges
 		return t.card('all')
 		.then(function(card){
-			var cardActions = [];
 			// request array of card activity
 			return fetch(`https://api.trello.com/1/cards/${card.id}/actions?key=%%TRELLO_KEY%%&token=%%TRELLO_TOKEN%%`)
 			.then(response => response.json())
 			.then(data => {
-				console.log(data);
+				console.log(card);
 				var cardMoves = data.filter(isCardMoveAction);
 				// put 0 days if there is no activity history
 				if (cardMoves.length == 0) {
-					return Math.round((Date.now() - Date.parse(data.date)) / (1000 * 60 * 60 * 24))
+					return Math.round((Date.now() - Date.parse(card.date)) / (1000 * 60 * 60 * 24))
 				}
 				// figure out days between today and entry date
 				console.log(Date.now());
